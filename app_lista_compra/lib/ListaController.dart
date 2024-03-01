d006dd5d3f21aa130c57275cd9d2781e7e9cacce
anthonyrct <anthonyteixaira@gmail.com>
2024-03-01 15:13:39 -0300

    01/03/24

--- app_lista_compra/lib/ListaController.dart
+++ app_lista_compra/lib/ListaController.dart
@@ -9,28 +9,38 @@ class ListaController extends ChangeNotifier {
 
   // Método para adicionar uma nova tarefa à lista
   void adicionarCompra(String descricao) {
-    // Converte a nova descrição para minúsculas
-    String descricaoLowerCase = descricao.toLowerCase();
+    if (descricao.trim() != '') {
+      // Convertendo a descrição atual e as descrições existentes na lista para minúsculas
+      String descricaoMinuscula = descricao.trim().toLowerCase();
+      List<String> descricoesExistentes =
+          _compras.map((compra) => compra.descricao.toLowerCase()).toList();
 
-    // Verifica se a compra já existe na lista
-    bool compraExistente = _compras
-        .any((compra) => compra.descricao.toLowerCase() == descricaoLowerCase);
+      // Verifica se a descrição já existe na lista de compras (em minúsculas)
+      bool jaExiste = descricoesExistentes.contains(descricaoMinuscula);
 
-    // Se a compra não existir, adicione à lista
-    if (!compraExistente) {
-      _compras.add(Listas(descricao, false));
+      // Se a compra já existe notifica para o usuario
+      if (jaExiste) {
+        print("essa compra já foi adicionada!");
+        return;
+      }
+
+      DateTime dataHoraAtual = DateTime.now(); // Obtém a data e hora atuais
+      _compras.add(Listas(descricao.trim(), false, dataHoraAtual));
+
       // Notifica os ouvintes (widgets) sobre a mudança no estado
       notifyListeners();
-    } else {
-      // Aqui você pode mostrar uma mensagem de erro ou tomar outra ação, se desejar
-      print('Essa compra já foi adicionada!');
     }
   }
 
   // Método para marcar uma compra como concluída com base no índice
   void marcarComoConcluida(int indice) {
     if (indice >= 0 && indice < _compras.length) {
-      _compras[indice].concluida = true;
+      if (_compras[indice].concluida) {
+        _compras[indice].concluida = false;
+      } else {
+        _compras[indice].concluida = true;
+      }
+
       // Notifica os ouvintes sobre a mudança no estado
       notifyListeners();
     }
@@ -40,6 +50,14 @@ class ListaController extends ChangeNotifier {
   void excluirCompra(int indice) {
     if (indice >= 0 && indice < _compras.length) {
       _compras.removeAt(indice);
+      // Notifica os ouvintes sobre a mudança no estado
+      notifyListeners();
+    }
+  }
+
+  void atualizarQuantidade(int indice, int quantidade) {
+    if (indice >= 0 && indice < _compras.length) {
+      _compras[indice].quantidade = quantidade;
       // Notifica os ouvintes sobre a mudança no estado
       notifyListeners();
     }
--- app_lista_compra/lib/ListaModel.dart
+++ app_lista_compra/lib/ListaModel.dart
@@ -2,7 +2,9 @@ class Listas {
   //atributos
   String descricao;
   bool concluida;
+  int quantidade;
+  DateTime dataHora;
 
   //Construtor da classe
-  Listas(this.descricao, this.concluida);
+  Listas(this.descricao, this.concluida, this.dataHora,{this.quantidade = 1});
 }
